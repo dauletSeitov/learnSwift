0529ba552b0d18d0550fe99a329f379a1ab1d109
skynet <seilov.skynet@gmail.com>
2018-12-04 18:17:14 +0600

    arrays

--- ex.swift
+++ ex.swift
@@ -1,1 +1,17 @@
+//arrays
 print("hello world");
+
+var arVar = [1,2,3,4,5,6]
+print("arVar: \(arVar)")
+
+var arInt:[Int] = [1,2,3,4,5,6]
+print("arInt: \(arInt)")
+
+var arInt2 = [Int]()
+print("arInt2: \(arInt2)")
+
+var arFloat:[Float] = [Float](repeating: 3, count: 5)
+print("arFloat: \(arFloat)")
+arFloat += [34, 12]
+arFloat.append(12);
+print("arFloat: \(arFloat)")
